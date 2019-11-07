04887b5a271fc1f84a62afd0f63c852feecd05f5
Alex Lee <hyunho98@gmail.com>
2019-11-07 22:29:23 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,6 +5,7 @@ def line(line)
   output = "The line is currently:"
   if line.size == 0
     puts "The line is currently empty."
+    break
   else
     line.each{|name, index|
     output = "#{output} #{index + 1}. #{name}"}
