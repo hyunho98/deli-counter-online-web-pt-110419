f01f64281c1c8106b4d00f3e0e959278a0d9bc55
Alex Lee <hyunho98@gmail.com>
2019-11-07 22:38:23 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,3 +16,12 @@ def take_a_number(line, name)
   line.push(name)
   puts "Welcome, #{name}. You are number #{line.size} in line."
 end
+
+def now_serving(line)
+  if line.size == 0
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{line[0]}."
+    line.shift
+  end
+end
