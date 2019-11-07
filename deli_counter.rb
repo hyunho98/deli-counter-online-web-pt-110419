7bb64f52954046f2bdcebafe9593e94fbc1ee01b
Alex Lee <hyunho98@gmail.com>
2019-11-07 22:27:05 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
 # Write your code here.
+katz_deli = []
+
+def line(line)
+  output = "The line is currently:"
+  if line.size == 0
+    puts "The line is currently empty."
+  else
+    line.each{|name, index|
+    output += " #{index + 1}. #{name}"}
+  end
+ output
+end
